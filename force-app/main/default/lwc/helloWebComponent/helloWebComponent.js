98ca48512456b660479994a8d7d079aa1cc04ee2
Eric <ericdaniel@wizsolucoes.com.br>
2019-10-25 17:57:56 -0300

    added functions on the webComponent

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,4 +1,7 @@
 import { LightningElement, track } from 'lwc';
 export default class HelloWebComponent extends LightningElement {
-	@track greeting = 'Trailblazer';
+    @track greeting = 'Trailblazer';
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
 }
