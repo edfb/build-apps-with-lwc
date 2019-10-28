81b182dfe569b8664024efaf0bef3c24ea7f0bcd
Eric <ericdaniel@wizsolucoes.com.br>
2019-10-28 11:56:26 -0300

    refactoring

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,15 +1,16 @@
 import { LightningElement, track, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import { loadStyle } from 'lightning/platformResourceLoader';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends LightningElement {
 	@track searchTerm = '';
 	@wire(searchBears, {searchTerm: '$searchTerm'})
 	bears;
-	appResources = {
-		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
-	};
 	handleSearchTermChange(event) {
+		connectedCallback() {
+			loadStyle(this, ursusResources + '/style.css');
+		}
 		// Debouncing this method: do not update the reactive property as
 		// long as this function is being called within a delay of 300 ms.
 		// This is to avoid a very large number of Apex method calls.
